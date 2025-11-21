ff5b7d016e68adb3e9e4892470da8b0db6f42338
a-sota <souta01.22@icloud.com>
2025-11-22 03:09:51 +0900

    0014_longest_common_prefix,py

---
+++ array/0014_longest_common_prefix.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        shortest = min(strs, key = len)
+
+        for i,ch in enumerate(shortest):
+            for s in strs:
+                if ch != s[i]:
+                    return shortest[:i]
+        return shortest        
+
+
+def test():
+    s = Solution()
+    assert s.longestCommonPrefix(["flower","flow","flight"]) == "fl"
+    assert s.longestCommonPrefix(["dog","racecar","car"]) == ""
+    print("OK")
+
+
+if __name__ == "__main__":
+    test()
